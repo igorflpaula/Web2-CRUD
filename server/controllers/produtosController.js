838a817f6bf3dccc529e224d7e2c658ae9ecb6e6
igorflpaula <igor_paula12@hotmail.com>
2022-02-15 08:54:50 -0300

    CRUD SEM LOGIN

--- server/controllers/produtosController.js
+++ server/controllers/produtosController.js
@@ -7,7 +7,7 @@ let connection = mysql.createConnection({
     database:  process.env.DB_NAME
 });
 
-// PRODUTOS
+// PRODUTOS CRUD - CREATE READ UPDATE DELETE
 
 // READ
 exports.view = (req,res) => {
@@ -15,7 +15,8 @@ exports.view = (req,res) => {
     connection.query('SELECT * FROM produtos WHERE situacao = "ativo"', (err,rows) => {
 
         if(!err){
-            res.render('home', { rows });
+            let produtoRemovido = req.query.removed;
+            res.render('home', { rows,  produtoRemovido});
         }
         else{
             console.log(err);
@@ -44,7 +45,7 @@ exports.find = (req,res) => {
     });
 };    
 
-// CRIAÇÃO
+// CRIATE
 exports.form = (req,res) => {
     res.render('addproduto');
 }
@@ -63,4 +64,93 @@ exports.create = (req,res) => {
 
         console.log('Conteúdo da tabela Produtos:\n', rows);
     });
-}
+};
+
+// EDIÇÃO
+exports.edit = (req,res) => {
+
+    connection.query('SELECT * FROM produtos WHERE id = ?',[req.params.id], (err,rows) => {
+
+        if(!err){
+            res.render('editproduto', { rows });
+        }
+        else{
+            console.log(err);
+        }
+
+        console.log('Conteúdo da tabela Produtos:\n', rows);
+    });
+};
+
+// UPDATE
+exports.update = (req,res) => {
+
+    const { nome, preco, descricao} = req.body;
+
+    connection.query('UPDATE produtos SET nome = ?, preco = ?, descricao = ? WHERE id = ?',[nome, preco, descricao, req.params.id], (err,rows) => {
+
+        if(!err){
+            connection.query('SELECT * FROM produtos WHERE id = ?',[req.params.id], (err,rows) => {
+
+                if(!err){
+                    res.render('editproduto', { rows, alert: `${nome} editado com sucesso!` });
+                }
+                else{
+                    console.log(err);
+                }
+        
+                console.log('Conteúdo da tabela Produtos:\n', rows);
+            });
+        }
+        else{
+            console.log(err);
+        }
+
+        console.log('Conteúdo da tabela Produtos:\n', rows);
+    });
+};
+
+// DELETE
+
+exports.delete = (req,res) => {
+
+    // connection.query('DELETE FROM produtos WHERE id = ?',[req.params.id], (err,rows) => {
+
+    //     if(!err){
+    //         res.redirect('/');
+    //     }
+    //     else{
+    //         console.log(err);
+    //     }
+
+    //     console.log('Conteúdo da tabela Produtos:\n', rows);
+    // });
+
+    connection.query('UPDATE produtos SET situacao = ? WHERE id = ?',['esgotado', req.params.id], (err,rows) => {
+
+        if(!err){
+            let produtoRemovido = encodeURIComponent('Produto removido com sucesso.');
+
+            res.redirect('/?removed= ?' + produtoRemovido);
+        }
+        else{
+            console.log(err);
+        }
+
+        console.log('Conteúdo da tabela Produtos:\n', rows);
+    });
+};
+
+// DETALHES
+
+exports.viewall = (req, res) => {
+    connection.query('SELECT * FROM produtos WHERE id = ?', [req.params.id], (err, rows) => {
+      if (!err) {
+        res.render('viewproduto', { rows });
+      } else {
+        console.log(err);
+      }
+      console.log('Conteúdo da tabela Produtos: \n', rows);
+    });
+  
+  }
--- server/routes/rotas.js
+++ server/routes/rotas.js
@@ -5,8 +5,12 @@ const userController = require('../controllers/userController');
 
 router.get('/', produtosController.view);
 router.post('/', produtosController.find);
+router.get('/:id', produtosController.delete);
 router.get('/addproduto', produtosController.form);
-router.post('/addproduto', produtosController.create);
+router.get('/editproduto/:id', produtosController.edit);
+router.post('/editproduto/:id', produtosController.update);
+router.get('/viewproduto/:id', produtosController.viewall);
+
 
 //router.get('/', userController.view);
 
